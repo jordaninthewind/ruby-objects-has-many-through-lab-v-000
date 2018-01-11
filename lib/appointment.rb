7b648f850ab59045b4313f68660176815f5215a5
Jordan Kline <jordan.kline@gmail.com>
2018-01-11 17:53:15 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class Appointment
-attr_reader :date, :doctor
+attr_reader :date, :doctor, :patient
 
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
   end
 
-  
+
 end
