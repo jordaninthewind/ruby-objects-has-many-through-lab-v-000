31f5b14e79dff54a5cfa3c606051acd86340c477
Jordan Kline <jordan.kline@gmail.com>
2018-01-11 17:02:08 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Genre
+attr_accessor :songs
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+
+  end
+
+  def artists
+    self.songs.collect do |i|
+      i.artist
+    end
+  end
+
+end
