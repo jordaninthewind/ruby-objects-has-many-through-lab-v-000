5e54c38c9f0f8b6ec7a16992f2122f9f4e7596ea
Jordan Kline <jordan.kline@gmail.com>
2018-01-11 18:15:12 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,12 +6,14 @@ attr_reader :name, :appointments
   def initialize(name)
     @name = name
     @appointments = []
+    @doctors = []
   end
 
   def add_appointment(appointment)
     @appointments << appointment
     appointment.patient = self
     appointment.doctor.appointments << appointment
+    self.doctors <<
     # binding.pry
   end
 
